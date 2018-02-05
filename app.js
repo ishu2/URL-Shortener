f5138952256da615ce3bbca8ba9264c50d314df3
ishu2 <rathiishita123@gmail.com>
2018-02-05 17:19:23 +0530

    Resolve regex checking

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express=require('express');
 var bodyParser=require('body-parser');
 var cors=require('cors');
 var mongoose=require('mongoose');
-var shortUrl=require('./models/shortUrl');
+var Url=require('./models/shortUrl');
+var encoder=require('./encoder/encode');
 var app=express();
 
 app.use(bodyParser.json());
@@ -11,51 +12,53 @@ app.use(express.static(__dirname+'/public'));
 
 app.set('view engine','ejs');
 
-mongoose.connect('mongodb://localhost/shortUrl');
+mongoose.connect('mongodb://localhost/Url',function(){
+    console.log("database connected...");
+});
 
 app.get('/',function(req,res){
     res.render('home');
 })
 
-app.post('/new',function(req,res,next){
-    var urlToShorten=req.params.urlToShorten;
-    var regex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-    if(regex.test(urlToShorten)===true){
-       var short=Math.floor(Math.random()*100000).toString();
-       var data=new shortUrl({
-           originalUrl: urlToShorten,
-           shorterUrl: short
-       });
+app.post('/shorten',function(req,res){
+    var originalUrl=req.body.url;
 
-       data.save(err=>{
-           if(err){
-               return res.send("ERROR!!");
-           }
-       });
-
-     res.render('short',{value:"http://localhost:1000/"+data.shorterUrl});
+    var regex=/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/
+    if(!regex.test(originalUrl)) { 
+        console.log("ERROR !!!!") 
+      res.render('error');
+    }else{
+        var shortenURL=encoder(originalUrl);
+        var newUrl=new Url({
+            originalUrl:originalUrl,
+            shorterUrl:shorterUrl
+        }).save(function(err){
+            if(err){
+                console.log(err);
+            }
+            var shortenFullUrl=req.protocol+'://localhost:1000/'+shortenUrl;
+            res.render('short',{shortenUrl:shortenFullUrl});
+        });
     }
-    var data=new shortUrl({
-        originalUrl: urlToShorten,
-        shorterUrl: 'InvalidURL'
-    });
-    res.json(data.shorterUrl);
-});
+})
 
 // Find in database and redirect to originalUrl
-app.get('/:urlToForward',function(req,res,next){
-    var shorterUrl=req.params.urlToForward;
-
-    shortUrl.findOne({'shorterUrl':shorterUrl},function(err,data){
-        if(err) return res.send('Error!!');
-        var re=new RegExp('^(http|https)://','i');
-        var str=data.originalUrl;
-        if(re.test(str)){
-            res.redirect(301,data.originalUrl);
-        }else{
-            res.redirect(301,'http://'+data.originalUrl);
+app.get('/:shortenUrl',function(req,res){
+    var shortenUrl=req.params.shortenUrl;
+    Url.findOne({shortenUrl:shortenURL},function(err,data){
+        if(err){
+            console.log(err);
         }
-    });
+        if(data==null){
+            res.render('home');
+        }else{
+            if(data.originalUrl[0]=='w'){
+                res.redirect('http://'+data.originalUrl);
+            }else{
+                res.redirect(data.originalUrl);
+            }
+        }
+    })
 })
 
 app.listen(1000,function(){
---
+++ encoder/encode.js
@@ -0,0 +1,19 @@
+var hasha=require('hasha');
+var encode = function(url) {
+  ///using hasha to generate unique 3 digit letter:)
+var hash=hasha(url);
+
+///randomly choose 4 letters on hash by generating a number;
+var loop=3;
+var randomNumber;
+var shortenUrl=''
+
+while(loop){
+  randomNumber=Math.floor((Math.random() * 100));
+  shortenUrl+=hash[randomNumber];
+  loop=loop-1;
+}
+return shortenUrl;
+}
+
+module.exports=encode;
